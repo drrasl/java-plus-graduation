f1ad18a023a873f7cf25d2ef7c06b38342d1cad8
Ruslan S. <ruslan.drrasl@yandex.ru>
2025-12-12 16:27:01 +0300

    Rev.2 - Добавил Дискавери, конфиг-сервер, гейтвэй, доработал main-service и stats-server

--- stats/client/src/main/java/ru/practicum/stats/client/configuration/ClientConfig.java
+++ stats/client/src/main/java/ru/practicum/stats/client/configuration/ClientConfig.java
@@ -2,6 +2,7 @@ package ru.practicum.stats.client.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.practicum.stats.client.StatClient;
@@ -9,8 +10,9 @@ import ru.practicum.stats.client.StatClient;
 @Configuration
 public class ClientConfig {
     @Bean
-    public StatClient statClient(@Value("${stat-server.url}") String serverUrl,
+    public StatClient statClient(@Value("${stats-server.id:stats-server}") String statsServiceId,
+                                 DiscoveryClient discoveryClient,
                                  RestTemplateBuilder builder) {
-        return new StatClient(serverUrl, builder);
+        return new StatClient(statsServiceId, discoveryClient, builder);
     }
 }
